8f6fedf9863b7c1bc53e0d48b5dcc012ff807f81
H-I <deathuntil5@gmail.com>
2023-07-31 03:20:38 +0100

    toggle the menu buton

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Main from './Main';
 // import Skills from './Skills';
 // import About from './About';
 // import Testimonials from './Testimonials';
 // import { Route, Routes } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
-import { AiOutlineMenu } from 'react-icons/ai';
+import { HiMenu, HiOutlineMenuAlt1 } from 'react-icons/hi';
 
 function Navbar(props) {
     const about = document.querySelector('.about');
     const skills = document.querySelector('.skills');
     const testimo = document.querySelector('.testimonials');
 
+    //toggle class menu
+    const [active, setActive] = useState(false);
+    const handle = () => { setActive(!active) };
+
     return (
         <>
             <nav className="navbar navbar-expand-lg ">
                 <div className="container">
-                    <NavLink className="navbar-brand font-monospace" to="#">H.I</NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <AiOutlineMenu />
+                    <button onClick={handle} className="navbar-toggler p-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        {active ? <HiOutlineMenuAlt1 /> : <HiMenu />}
                     </button>
+                    <NavLink className="navbar-brand font-monospace" to="#">H.I</NavLink>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav d-flex align-items-start align-items-lg-center ms-auto me-auto mb-2 mb-lg-0">
                             <li className="nav-item me-4">
@@ -36,7 +40,7 @@ function Navbar(props) {
                             </li>
                         </ul>
                         <form className="d-flex contact">
-                            <NavLink className="btn text-align" to="/Contact">Contact</NavLink>
+                            <NavLink className="btn text-align" to={'https://www.facebook.com/profile.php?id=100092187097027&mibextid=ZbWKwL'} target='_blank'>Contact</NavLink>
                         </form>
                     </div>
                 </div>
