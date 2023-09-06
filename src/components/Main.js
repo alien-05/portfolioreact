bdbdb7f9f3eb988a450ef431bc5b682fb7fcbee9
H-I <deathuntil5@gmail.com>
2023-09-07 00:24:18 +0100

    finish hero section -animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import Typed from 'react-typed';
-import { AiOutlineInstagram, AiFillGithub } from "react-icons/ai";
-import { FaFacebook } from "react-icons/fa";
-// import { BiLogoGmail } from "react-icons/bi";
+import { AiOutlineInstagram, AiFillGithub, AiFillHtml5 } from "react-icons/ai";
+import { FaFacebook, FaReact, FaBootstrap } from "react-icons/fa";
+import { DiCss3, DiJavascript1, DiSass } from "react-icons/di";
+import { SiVite, SiRedux } from "react-icons/si";
+import { TbBrandVscode } from "react-icons/tb";
+
 import logo from '../../src/imgs/Background Image.png';
 
 function Main(props) {
@@ -32,7 +35,7 @@ function Main(props) {
                 <div className="container">
                     <div className="row mt-0 ">
                         <div className="col-12 left col-lg-6">
-                            <h1 className='title fs-1 text-center text-lg-start fw-bold pt-3 mb-0'>H.I WEB DEVELOPER </h1>
+                            <h1 className='title fs-1 text-center text-lg-start fw-bold  mb-0'>H.I WEB DEVELOPER </h1>
                             <span className="fonction fw-bold d-flex justify-content-center justify-content-lg-start fs-3 mt-0 mb-1">
                                 <span className="me-2">I'm a</span><Typed
                                     strings={["Web Designer", "Web Developer", "WordPress Developer"]}
@@ -41,12 +44,12 @@ function Main(props) {
                                     loop
                                 />
                             </span>
-                            <p className="description  lh-base font-monospace pt-3 pb-0 pb-lg-1 text-white-50 text-capitalize">
+                            <p className="description  lh-base font-monospace pt-2 pb-0  text-white-50 text-capitalize">
                                 As a web developer, I am responsible for designing and developing web pages. My primary focus is to create responsive, user-friendly experiences that meet the needs of a diverse online audience.
                             </p>
                             <div className="col-12 icons-social">
-                                <Link className="instagram fs-3 " to={'https://instagram.com/hi__webdesign?igshid=ZDc4ODBmNjlmNQ=='} target="_blank"><AiOutlineInstagram /></Link>
-                                <Link className="github fs-3 " to={'https://github.com/alien-05'} target="_blank"><AiFillGithub /></Link>
+                                <Link className="instagram fs-3 me-3 " to={'https://instagram.com/hi__webdesign?igshid=ZDc4ODBmNjlmNQ=='} target="_blank"><AiOutlineInstagram /></Link>
+                                <Link className="github fs-3 me-3 " to={'https://github.com/alien-05'} target="_blank"><AiFillGithub /></Link>
                                 {/* <Link className="gmail fs-3 me-4" to='hi.webdesigncompany@gmail.com'><BiLogoGmail /></Link> */}
                                 <Link className="facebook fs-4 " to={'https://www.facebook.com/profile.php?id=100092187097027&mibextid=ZbWKwL'} target="_blank"><FaFacebook /></Link>
                             </div>
@@ -59,27 +62,38 @@ function Main(props) {
                             </div>
                         </div>
                     </div>
-                    <div className="footer ps-5 d-flex align-items-center row w-100 position-absolute">
-                        <div className="col-auto">
-                            <div className="exp d-flex align-items-center me-5 ">
-                                <span className="num text-light  fw-bold me-2 mb-1">3</span>
-                                <span className="txt text-uppercase">years of <br /> experience</span>
+                    <div className="footer  d-flex align-items-center justify-content-center  w-100 position-absolute">
+                        <div className="row">
+
+                            <div className="col-6 col-lg-auto">
+                                <div className="exp d-flex justify-content-center justify-content-lg-start align-items-center me-5 ">
+                                    <span className="num text-light  fw-bold me-2 mb-1">3</span>
+                                    <span className="txt text-uppercase">years of <br /> experience</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-auto">
-                            <div className="prjt d-flex align-items-center me-5">
-                                <span className="num text-light mb-1 fw-bold me-2">12</span>
-                                <span className="txt text-uppercase">Projects Completed <br /> around the world</span>
+                            <div className="col-6 col-lg-auto">
+                                <div className="prjt d-flex justify-content-center justify-content-lg-start align-items-center me-5">
+                                    <span className="num text-light mb-1 fw-bold me-2">12</span>
+                                    <span className="txt text-uppercase">Projects Completed <br /> around the world</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-auto">
-                            <div className="tools d-flex align-items-center mb-2" >
-                                <span className="me-5 fs-1">x</span>
-                                <span className="me-5  fs-1">x</span>
-                                <span className="me-5 fs-1">x</span>
-                                <span className="me-5 fs-1">x</span>
-                                <span className="me-5 fs-1">x</span>
-                                <span className="me-4 fs-1">x</span>
+                            <div className="col-md-12 col-lg-auto">
+                                <div className="tools d-flex justify-content-center flex-column flex-xl-row align-items-center mt-1" >
+                                    <div className="div">
+                                        <span className="html me-5  fs-1"><AiFillHtml5 /></span>
+                                        <span className="css me-5  fs-1"><DiCss3 /></span>
+                                        <span className="js me-5 fs-1"><DiJavascript1 /></span>
+                                        <span className="react me-5 fs-1"><FaReact /></span>
+                                        <span className="bp me-xl-5 fs-1"><FaBootstrap /></span>
+                                    </div>
+
+                                    <div className="div">
+                                        <span className="sass me-5 ms-0 fs-1"><DiSass /></span>
+                                        <span className="vite me-5 fs-1"><SiVite /></span>
+                                        <span className="redux me-5 fs-1"> <SiRedux /></span>
+                                        <span className="vscode fs-1"><TbBrandVscode /></span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
